75db33251dd20837a2eba29bc7b8c148d58436fc
Nigel <nigel@resynctech.com>
2020-05-17 12:58:10 +0800

    feat(nodejs): added backend

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,92 @@
+import {
+    Resolver,
+    Query,
+    Mutation,
+    Arg,
+    ObjectType,
+    Field,
+    Ctx,
+    UseMiddleware,
+    Int,
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
+import { User } from "./entity/User";
+import { MyContext } from "./MyContext";
+import { createRefreshToken, createAccessToken } from "./auth";
+import { isAuth } from "./isAuth";
+import { sendRefreshToken } from "./sendRefreshToken";
+import { getConnection } from "typeorm";
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string;
+}
+
+@Resolver()
+export class UserResolver {
+    @Query(() => String)
+    hello() {
+        return "Hello";
+    }
+
+    @Query(() => String)
+    @UseMiddleware(isAuth)
+    bye(@Ctx() { payload }: MyContext) {
+        return `your user id is: ${payload!.userId}`;
+    }
+
+    @Query(() => [User])
+    users() {
+        return User.find();
+    }
+
+    @Mutation(() => Boolean)
+    async revokeRefreshTokenForUser(@Arg("userId", () => Int) userId: number) {
+        await getConnection()
+            .getRepository(User)
+            .increment({ id: userId }, "tokenVersion", 1);
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg("email") email: string,
+        @Arg("password") password: string,
+        @Ctx() { res }: MyContext
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            throw new Error("Could not find user");
+        }
+
+        const valid = await compare(password, user.password);
+
+        if (!valid) {
+            throw new Error("bad password");
+        }
+
+        sendRefreshToken(res, createRefreshToken(user));
+
+        return {
+            accessToken: createAccessToken(user),
+        };
+    }
+
+    @Mutation(() => Boolean)
+    async register(
+        @Arg("email") email: string,
+        @Arg("password") password: string
+    ) {
+        const hashedPassword = await hash(password, 12);
+        try {
+            await User.insert({
+                email,
+                password: hashedPassword,
+            });
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+}
---
+++ server/src/index.ts
@@ -0,0 +1,79 @@
+import "reflect-metadata";
+import { config } from "dotenv";
+import { createConnection } from "typeorm";
+// import { User } from "./entity/User";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { sendRefreshToken } from "./sendRefreshToken";
+
+(async () => {
+    const app = express();
+    app.use(cookieParser());
+    config();
+    app.get("/", (_, res) => res.send("ok"));
+    app.post("/refresh_token", async (req, res) => {
+        const token = req.cookies.jid;
+        if (!token) {
+            return res.send({ ok: false, accessToken: "" });
+        }
+
+        let payload: any = null;
+        try {
+            payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+        } catch (err) {
+            console.log(err);
+            return res.send({ ok: false, accessToken: "" });
+        }
+
+        const user = await User.findOne({ id: payload.userId });
+
+        if (!user) {
+            return res.send({ ok: false, accessToken: "" });
+        }
+
+        if (user.tokenVersion !== payload.tokenVersion) {
+            return res.send({ ok: false, accessToken: "" });
+        }
+        sendRefreshToken(res, createRefreshToken(user));
+
+        return res.send({ ok: true, accessToken: createAccessToken(user) });
+    });
+    await createConnection();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver],
+        }),
+        context: ({ req, res }) => ({ req, res }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+    app.listen(4001, () => {
+        console.log("Express Server started!");
+    });
+})();
+// createConnection()
+//     .then(async (connection) => {
+//         console.log("Inserting a new user into the database...");
+//         const user = new User();
+//         user.firstName = "Timber";
+//         user.lastName = "Saw";
+//         user.age = 25;
+//         await connection.manager.save(user);
+//         console.log("Saved a new user with id: " + user.id);
+
+//         console.log("Loading users from the database...");
+//         const users = await connection.manager.find(User);
+//         console.log("Loaded users: ", users);
+
+//         console.log(
+//             "Here you can setup and run express/koa/any other framework."
+//         );
+//     })
+//     .catch((error) => console.log(error));
